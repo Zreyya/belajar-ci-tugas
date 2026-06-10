bc6792e275b24aa0ec66b83dd4e320f90bfa88b4
Zreyya <zreyrafa@gmail.com>
2026-06-10 07:38:09 +0700

    first commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ProductModel; 
+
+class Home extends BaseController
+{   
+    protected $productModel;
+    function __construct(){
+        helper(['number', 'form']);
+        $this->productModel = new ProductModel();
+}
+    public function index()
+    {
+        $products = $this->productModel->findAll();
+        $data['products'] = $products;
+
+        return view('v_home', $data);
+    }
+
+    public function contact(): string
+    {
+        return view('v_contact');
+    }
+        public function faq()
+    {
+        return view('v_faq');
+    }
+}
